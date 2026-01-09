2cfaf88ecea4838bf3e9db4924a644bf8ab30835
jonicrafthd23-alt <jonicrafthd23@gmail.com>
2026-01-09 22:02:55 +0100

    idk

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -26,7 +26,7 @@ import org.apache.logging.log4j.Logger;
 @SuppressWarnings("removal")
 public class ExampleMod {
 
-    public static final String MOD_ID = "examplemod";
+    public static final String MOD_ID = "luna";
     public static final Logger LOGGER = LogManager.getLogger();
     public static GTRegistrate EXAMPLE_REGISTRATE = GTRegistrate.create(ExampleMod.MOD_ID);
 
